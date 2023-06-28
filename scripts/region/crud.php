daf2c6a72076df05f6551f6f26bf6a75a5e83507
mariaCubides <mcubides1@udi.edu.co>
2023-06-28 16:14:24 -0500

    feat: :sparkles: Tercer crud de la tabla region realizado
    Se realiza el crud completo de la tabla region

--- scripts/region/crud.php
+++ scripts/region/crud.php
@@ -8,30 +8,30 @@
             $this->_DATA =(file_get_contents('php://input')=="") ? ["Mensaje"=>"Envien datos"] : json_decode(file_get_contents('php://input',true));
         }
         public function getAll(){
-            $res = $this->con->prepare("SELECT * FROM eps");
+            $res = $this->con->prepare("SELECT * FROM region");
             $res->execute();
             echo json_encode($res->fetchAll(\PDO::FETCH_ASSOC));
         }
         public function postAll() {
-            $res = $this->con->prepare("INSERT INTO eps(IdEps, NombreEps, DireccionEps) VALUES(:id, :nomEps, :dirEps)");
+            $res = $this->con->prepare("INSERT INTO region(idRegion, nombreReg, idDep) VALUES(:id, :nomReg, :idDep)");
             $res->bindParam(':id', $this->_DATA->id);
-            $res->bindParam(':nomEps', $this->_DATA->nomEps);
-            $res->bindParam(':dirEps', $this->_DATA->dirEps);
+            $res->bindParam(':nomReg', $this->_DATA->nomReg);
+            $res->bindParam(':idDep', $this->_DATA->idDep);
             $res->execute();
             print_r($res->rowCount());
         }
         public function putAll(){
-            $res = $this->con->prepare("UPDATE eps SET NombreEps = :nomEps, DireccionEps = :dirEps WHERE IdEps = :id");
+            $res = $this->con->prepare("UPDATE region SET nombreReg = :nomReg, idDep = :idDep WHERE idRegion = :id");
             $res->bindParam(':id', $this->_DATA->id);
-            $res->bindParam(':nomEps', $this->_DATA->nomEps);
-            $res->bindParam(':dirEps', $this->_DATA->dirEps);
+            $res->bindParam(':nomReg', $this->_DATA->nomReg);
+            $res->bindParam(':idDep', $this->_DATA->idDep);
             $res->execute();
             print_r($res->rowCount());
         }
         public function deleteAll(){
             $cox = new \App\connect();
             $_DATA = json_decode(file_get_contents("php://input"));
-            $res = $cox->con->prepare("DELETE FROM eps WHERE IdEps=:id");
+            $res = $cox->con->prepare("DELETE FROM region WHERE idRegion=:id");
             $res->bindParam(':id', $_DATA->id);
             $res->execute();
             print_r($res->rowCount());
