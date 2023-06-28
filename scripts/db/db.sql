92cd1e44dcd84757ab0d759e2d9041ccefed8aae
mariaCubides <mcubides1@udi.edu.co>
2023-06-28 15:42:04 -0500

    feat: :rocket: se crea repositorio con todas las carpetas necesarias para empezar a implemetar.
    Se realiza la creacion de la BASE DE DATOS  db.sql con las tablas solicitadas

--- scripts/db/db.sql
+++ scripts/db/db.sql
@@ -1,75 +1,31 @@
 CREATE DATABASE campuslands;
 use campuslands;
-CREATE TABLE eps(
-    IdEps INT(11) NOT NULL PRIMARY KEY,
-    NombreEps VARCHAR(50) NOT NULL,
-    DireccionEps VARCHAR(30) NOT NULL
+CREATE TABLE pais(
+    idPais INT(11) NOT NULL PRIMARY KEY,
+    NombrePais VARCHAR(50) NOT NULL
+);
+ALTER TABLE pais MODIFY COLUMN nombrePais INT(50);
+
+
+CREATE TABLE departamento(
+    idDep INT(11) NOT NULL PRIMARY KEY,
+    nombreDep VARCHAR(50) NOT NULL,
+    idPais INT(11) NOT NULL,
+    FOREIGN KEY (idPais) REFERENCES pais(idPais)
 );
 
-CREATE TABLE ciudad(
-    IdCiudad INT(11) NOT NULL PRIMARY KEY,
-    NombreCiudad VARCHAR(50) NOT NULL,
-    Region VARCHAR(30) NOT NULL
+CREATE TABLE region(
+    idRegion INT(11) NOT NULL PRIMARY KEY,
+    nombreReg VARCHAR(60) NOT NULL,
+    idDep INT(11) NOT NULL,
+    FOREIGN KEY (idDep) REFERENCES departamento(idDep)
 );
 
-CREATE TABLE paciente(
-    IdPaciente INT(11) NOT NULL PRIMARY KEY,
-    NombrePaciente VARCHAR(50) NOT NULL,
-    FechaNacimiento DATE NOT NULL,
-    TipoSangre VARCHAR(10) NOT NULL,
-    IdEps INT(11) NOT NULL,
-    IdCiudad INT(11) NOT NULL,
-    FOREIGN KEY (IdEps) REFERENCES eps(IdEps),
-    FOREIGN KEY (IdCiudad) REFERENCES ciudad(IdCiudad)
-);
-
-CREATE TABLE bacteriologo(
-    IdBacteriologo INT(11) NOT NULL PRIMARY KEY,
-    NombreBacteriologo VARCHAR(50) NOT NULL,
-    FechaIngreso DATE NOT NULL
-);
-
-CREATE TABLE ordenes(
-    IdOrdenes INT(11) NOT NULL PRIMARY KEY,
-    NombreOrdenes VARCHAR(50) NOT NULL,
-    Medico VARCHAR(50) NOT NULL,
-    IdPaciente INT(11) NOT NULL,
-    IdBacteriologo INT(11) NOT NULL,
-    FOREIGN KEY (IdPaciente) REFERENCES paciente(IdPaciente),
-    FOREIGN KEY (IdBacteriologo) REFERENCES bacteriologo(IdBacteriologo)
-);
-
-CREATE TABLE examenes(
-    Codigo INT(11) NOT NULL PRIMARY KEY,
-    NombreExamenes VARCHAR(50) NOT NULL,
-    Costo INT(11) NOT NULL,
-    IdBacteriologo INT(11) NOT NULL,
-    FOREIGN KEY (IdBacteriologo) REFERENCES bacteriologo(IdBacteriologo)
-);
-
-CREATE TABLE reactivos(
-    IdReactivos INT(11) NOT NULL PRIMARY KEY,
-    NombreReactivos VARCHAR(50) NOT NULL,
-    Existencias INT(11) NOT NULL
-);
-
-CREATE TABLE examenes_reactivos(
-    IdExamenesReactivos INT(11) NOT NULL PRIMARY KEY,
-    Codigo INT(11) NOT NULL,
-    IdReactivos INT(11) NOT NULL,
-    FOREIGN KEY (Codigo) REFERENCES examenes(Codigo),
-    FOREIGN KEY (IdReactivos) REFERENCES reactivos(IdReactivos)
-);
-
-CREATE TABLE proveedor(
-    NitProveedor INT(11) NOT NULL PRIMARY KEY,
-    NombreProveedor VARCHAR(50) NOT NULL
-);
-
-CREATE TABLE reactivos_proveedor(
-    IdReactivosProveedor INT(11) NOT NULL PRIMARY KEY,
-    IdReactivos INT(11) NOT NULL,
-    NitProveedor INT(11) NOT NULL,
-    FOREIGN KEY (IdReactivos) REFERENCES reactivos(IdReactivos),
-    FOREIGN KEY (NitProveedor) REFERENCES proveedor(NitProveedor)
+CREATE TABLE campers(
+    idCamper VARCHAR(20) NOT NULL PRIMARY KEY,
+    nombreCamper VARCHAR(50) NOT NULL,
+    apellidoCamper VARCHAR(50) NOT NULL,
+    fechaNac DATE NOT NULL,
+    idReg INT(11) NOT NULL,
+    FOREIGN KEY (idReg) REFERENCES region(idRegion)
 );
